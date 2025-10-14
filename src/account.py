b98bd8f17079270ef753792f985d4aee70775eac
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2025-10-14 17:18:47 +0200

    Feature 2-5

--- src/account.py
+++ src/account.py
@@ -1,5 +1,36 @@
+import re
+
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, pesel, promo_code=None):
         self.first_name = first_name
         self.last_name = last_name
         self.balance = 0
+
+        if len(pesel) != 11:
+            self.pesel = "Invalid"
+        else:
+            self.pesel = pesel
+
+        gets_bonus = self.check_year_of_birth(self.pesel) 
+
+        pattern = r"^PROM_.+"
+
+        if promo_code and re.fullmatch(pattern, promo_code) and gets_bonus:
+            self.balance += 50
+
+    def check_year_of_birth(self, pesel):
+        if pesel == "Invalid":
+            return False
+        
+        month = int(pesel[2:4])
+
+        if month >= 21 and month <= 32:
+            year_of_birth = 2000 + int(pesel[:2])
+        elif month >= 41 and month <= 52:
+            year_of_birth = 2100 + int(pesel[:2])
+        elif month >= 61 and month <= 72:
+            year_of_birth = 2200 + int(pesel[:2])
+        else:
+            year_of_birth = 1900 + int(pesel[:2])
+
+        return year_of_birth > 1960
