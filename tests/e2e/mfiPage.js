90f752ca6b7725874748981ca5e2baecd0553f96
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2026-01-27 12:28:44 +0100

    Playwright

---
+++ tests/e2e/mfiPage.js
@@ -0,0 +1,36 @@
+import { expect } from "@playwright/test";
+
+export class MFIPage {
+  constructor(page) {
+    this.page = page;
+    this.workersLink = page
+      .getByLabel("Nagłówek")
+      .getByRole("link", { name: "Pracownicy" });
+    this.personnel = page
+      .locator(".views-element-container")
+      .getByRole("link", { name: "Skład osobowy" });
+  }
+
+  async goto() {
+    await this.page.goto("https://mfi.ug.edu.pl/");
+  }
+
+  async workers() {
+    await this.workersLink.click();
+    await this.personnel.click();
+  }
+
+  async searchWorker(search, expected) {
+    try {
+      await this.page.getByLabel("Imię lub nazwisko").fill(search);
+
+      await expect(
+        this.page.getByRole("link", { name: expected }),
+      ).toBeVisible();
+
+      await this.page.getByRole("link", { name: expected }).click();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
